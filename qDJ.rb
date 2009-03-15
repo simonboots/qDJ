9aadf7fb1e1936b5677932a1388d67d2e6da6296
Simon Stiefel <simon@stiefels.net>
2009-03-15 12:02:32 +0100

    initial commit

---
+++ qDJ.rb
@@ -0,0 +1,12 @@
+require 'getoptlong'
+require "fsdb"
+#require 'rdoc/usage'
+#
+#opts = GetoptLong.new(
+#    [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+#    [ '--repeat', '-n', GetoptLong::REQUIRED_ARGUMENT ],
+#    [ '--name', GetoptLong::OPTIONAL_ARGUMENT ]
+#  )
+#  
+#RDoc::usage
+#
